2fb12a84f54f76e00538ba3c87a263c93038cd76
vianarafaa <vianarafaa@gmail.com>
2026-03-24 16:23:29 -0300

    criação da seção faq

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const buttons = document.querySelectorAll('[data-tab-button]')
+    const questions = document.querySelectorAll('[data-faq-question]')
 
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', (btn) => {
@@ -13,7 +14,18 @@ document.addEventListener('DOMContentLoaded', () => {
             btn.target.classList.add('shows__tabs__button--is-active')
         })
     }
+
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', abreOuFechaResposta)
+    }
 })
+
+function abreOuFechaResposta(e) {
+    const classe = 'faq__questions__item--is-open'
+    const elementoPai = e.target.parentNode
+
+    elementoPai.classList.toggle(classe)
+}
 
 function removeBotaoAtivo() {
     const buttons = document.querySelectorAll('[data-tab-button]')
