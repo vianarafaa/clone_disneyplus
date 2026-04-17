355f3872b914128ccef3086173cbcf1d1a282173
vianarafaa <vianarafaa@gmail.com>
2026-04-17 10:00:27 -0300

    configura o uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
 const gulp = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
+const uglify = require('gulp-uglify')
 const imagemin = require('gulp-imagemin')
+
+function scripts() {
+    return gulp.src('./src/scripts/*.js').pipe(uglify()).pipe(gulp.dest('./dist/js'))
+}
 
 function styles() {
     return gulp
@@ -13,8 +18,9 @@ function images() {
     return gulp.src('./src/images/**/*').pipe(imagemin()).pipe(gulp.dest('./dist/images'))
 }
 
-exports.default = gulp.parallel(styles, images)
+exports.default = gulp.parallel(styles, images, scripts)
 
 exports.watch = () => {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts))
 }
